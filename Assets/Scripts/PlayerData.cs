ca1a29ca06be70c3ace3b26e1975b34fa6b313f7
JshReaper <jshchansen@gmail.com>
2019-09-10 12:40:37 +0200

    made json stuff and msg's system

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+class PlayerData
+{
+    public PlayerData(int id, Vector3 pos, Vector3 scale)
+    {
+        Id = id;
+        Pos = pos;
+        Scale = scale;
+    }
+    public int Id { get; }
+    public Vector3 Pos { get; }
+    public Vector3 Scale { get; }
+}
