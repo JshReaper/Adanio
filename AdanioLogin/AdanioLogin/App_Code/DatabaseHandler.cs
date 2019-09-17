bc2bb5e84233f88c83fa0a4c9b360167f236425d
JshReaper <jshchansen@gmail.com>
2019-09-17 12:21:31 +0200

    added database handler script

---
+++ AdanioLogin/AdanioLogin/App_Code/DatabaseHandler.cs
@@ -0,0 +1,28 @@
+﻿using MySql.Data;
+
+namespace AdanioLogin.App_Code
+{
+    public class DatabaseHandler
+    {
+        private static DatabaseHandler instance;
+        public static DatabaseHandler Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new DatabaseHandler();
+                }
+                return instance;
+            }
+        }
+        private DatabaseHandler()
+        {
+
+        }
+        public void Login()
+        {
+
+        }
+    }
+}
