6898938079eba562a1ed7462deee6eb155e91c24
TopperTheKnight <tobi1995@live.dk>
2019-09-18 13:07:16 +0200

    Service app created with logic

---
+++ CommunicationSystem/CommunicationSystem/CommunicationSystem/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Messaging;
+
+namespace CommunicationSystem
+{ 
+
+    class Program
+    {
+        static List<Connection> connections = new List<Connection>();
+        static string path = @"Direct=TCP:192.168.0.100\globalQueue";
+        static void Main(string[] args)
+        {
+            if (!MessageQueue.Exists(path))
+            {
+                Console.WriteLine("Creating new queue for service");
+                MessageQueue.Create(path,true);
+            }
+
+            MessageQueue reciever = new MessageQueue(path);
+
+            reciever.ReceiveCompleted += new ReceiveCompletedEventHandler(MyReceiveCompleted);
+
+            while (true)
+            {
+                reciever.BeginReceive();
+
+                
+            }
+
+
+        }
+
+        private static void MyReceiveCompleted(Object source, ReceiveCompletedEventArgs asyncResult)
+        {
+            try
+            {
+                // Connect to the queue.
+                MessageQueue mq = (MessageQueue)source;
+
+                // End the asynchronous receive operation.
+                Message m = mq.EndReceive(asyncResult.AsyncResult);
+               
+
+                //Command:From:Target:Message
+                string[] content = m.Body.ToString().Split(':');
+                string cmd = content[0];
+                string from = content[1];
+                string targetDest = content[2];
+                string message = content[3];
+
+
+                switch (cmd)
+                {
+                    case "NEW":
+                        Connection c = new Connection(from,message);
+                        connections.Add(c);
+                        Console.WriteLine("Received new connection: " + from + ", With name: " + message );
+                        break;
+
+                    case "MSG":
+                        foreach (var co in connections)
+                        {
+                            if(co.messageID == targetDest)
+                            {
+                                //Target exists
+                                Message ms = new Message(string.Format("MSG:{0}:{1}:{2}",from,targetDest,message));
+                                Console.WriteLine("Received a Message Command from: " + from + ", directod to: " + targetDest);
+                                mq.Path = co.address;
+                                mq.Send(ms);
+                            }
+                        }
+                        break;
+
+                    case "TARGETS":
+                        Console.WriteLine("A user requested the available connections: " + from);
+                        string targets = "";
+                        foreach (var co in connections)
+                        {
+                            if (co.address != from) {
+                                targets += co.messageID + ":";
+                            }
+                        }
+                        targets = targets.Remove(targets.Length - 1, 1);
+                        Message msg = new Message("TARGETS:" + targets);
+                        mq.Path = from;
+                        mq.Send(msg); 
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid command recieved from: " + m.SenderId.ToString());
+                        break;
+                }
+
+                // Restart the asynchronous receive operation.
+                mq.Path = path;
+                mq.BeginReceive();
+            }
+            catch (MessageQueueException e)
+            {
+                // Handle sources of MessageQueueException.
+                Console.WriteLine("ERROR: " + e);
+            }
+
+            // Handle other exceptions.
+            
+            return;
+        }
+
+    }
+    [System.Serializable]
+    class Connection
+    {
+        public Connection(string address, string messageID)
+        {
+            this.address = address;
+            this.messageID = messageID;
+        }
+
+        public string address;
+        public string messageID;
+    }
+    
+
+}
