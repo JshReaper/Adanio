c629a8c1279e30b1f646d3420d7143b410f6d8a4
TopperTheKnight <tobi1995@live.dk>
2019-09-27 13:10:01 +0200

    chat log

---
+++ Adanio game/Assets/TextTerminationTimer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextTerminationTimer : MonoBehaviour
+{
+    [SerializeField]
+    float lifeTime;
+    [SerializeField]
+    float readTime = 4;
+    float targetTime;
+    float fadeSpeed = 2f;
+    TMPro.TextMeshProUGUI text;
+    Color target = new Color(0, 0, 0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetTime = Time.time + readTime + lifeTime;
+        text = GetComponent<TMPro.TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > targetTime-lifeTime) {
+            text.color = Color.Lerp(text.color, target, fadeSpeed * Time.deltaTime);
+        }
+        if (Time.time > targetTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
