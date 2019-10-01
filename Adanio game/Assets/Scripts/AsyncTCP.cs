0c556ef593ef7465dc53a4da9af07bdcd26ae849
MythrilJojo <trier94@hotmail.com>
2019-10-01 10:40:35 +0200

    Added Async TCP Connection
    Deleted Bolt cause it fucked with Newtonsoft dll's.

---
+++ Adanio game/Assets/Scripts/AsyncTCP.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TCPOption;
+
+public class AsyncTCP : MonoBehaviour
+{
+    private AsynchronousSocketListener aListener;
+    private AsynchronousClient aClient;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void CreateSocketExample()
+    {
+        aListener = new AsynchronousSocketListener();
+    }
+
+    public void CreateClientExample()
+    {
+        aClient = new AsynchronousClient();
+
+        aClient.Send<string>(AsynchronousClient.Client, "Test message");
+        AsynchronousClient.SendDone.WaitOne();
+
+        aClient.Receive(AsynchronousClient.Client);
+        AsynchronousClient.ReceiveDone.WaitOne();
+        Debug.Log("Done");
+    }
+
+    private void OnApplicationQuit()
+    {
+        if (aListener != null)
+            aListener.Close();
+
+        if (aClient != null)
+            aClient.Close();
+    }
+}
