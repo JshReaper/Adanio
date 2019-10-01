31c4a50cfbd361279dee3abe6eb3a24326a263e8
JshReaper <jshchansen@gmail.com>
2019-10-01 12:10:32 +0200

    patchmanager fixed

--- AdanioLogin/PatchManager/ReleaseChecker.cs
+++ AdanioLogin/PatchManager/ReleaseChecker.cs
@@ -11,6 +11,43 @@ using System.Threading.Tasks;
 
 namespace PatchManager
 {
+    public class RealRelease : Release
+    {
+
+        public new string TarballUrl
+        {
+            get
+            {
+                return base.TarballUrl;
+            }
+            set
+            {
+                base.TarballUrl = value;
+            }
+        }
+        public new Author Author { get { return base.Author; }  set { base.Author = value; } }
+        public new DateTimeOffset? PublishedAt { get { return base.PublishedAt; }  set { base.PublishedAt = value; } }
+        public new DateTimeOffset CreatedAt { get { return base.CreatedAt; }  set { base.CreatedAt = value; } }
+        public new bool Prerelease { get { return base.Prerelease; }  set { base.Prerelease = value; } }
+        public new bool Draft { get { return base.Draft; }  set { base.Draft = value; } }
+        public new string Body { get { return base.Body; }  set { base.Body = value; } }
+        public new string Name { get { return base.Name; }  set { base.Name = value; } }
+        public new string TagName { get { return base.TagName; }  set { base.TagName = value; } }
+        public new string ZipballUrl { get { return base.ZipballUrl; }  set { base.ZipballUrl = value; } }
+        //
+        // Summary:
+        //     GraphQL Node Id
+        public new string NodeId { get { return base.NodeId; }  set { base.NodeId = value; } }
+        public new int Id { get { return base.Id; }  set { base.Id = value; } }
+        public new string UploadUrl { get { return base.UploadUrl; }  set { base.UploadUrl = value; } }
+        public new string AssetsUrl { get { return base.AssetsUrl; }  set { base.AssetsUrl = value; } }
+        public new string HtmlUrl { get { return base.HtmlUrl; }  set { base.HtmlUrl = value; } }
+        public new string Url { get { return base.Url; }  set { base.Url = value; } }
+        public new string TargetCommitish { get { return base.TargetCommitish; }  set { base.TargetCommitish = value; } }
+        public new IReadOnlyList<ReleaseAsset> Assets { get { return base.Assets; }  set { base.Assets = value; } }
+
+    }
+
     public class ReleaseChecker
     {
         private readonly string owner;
@@ -51,7 +88,7 @@ namespace PatchManager
             Release current = null;
             try
             {
-                current = JsonConvert.DeserializeObject<Release>(File.ReadAllText(path+"\\version.json"));
+                current = JsonConvert.DeserializeObject<RealRelease>(File.ReadAllText(path+"\\version.json")) as Release;
             }
             catch (Exception e)
             {
@@ -88,8 +125,8 @@ namespace PatchManager
         }
         async Task DownLoadReleaseAsync()
         {
-            if(latestrelease == null) { 
-            latestrelease = await client.Repository.Release.GetLatest(owner, reponame);
+            if(latestrelease == null) {
+                latestrelease = await client.Repository.Release.GetLatest(owner, reponame);
             }
             //Gets the assets for the latest relase
             var assets = await client.Repository.Release.GetAllAssets(owner, reponame, latestrelease.Id);
@@ -101,7 +138,7 @@ namespace PatchManager
 
                 await wc.DownloadFileTaskAsync(asset.BrowserDownloadUrl, filename);
             }
-            File.WriteAllText(path + "\\version.json", JsonConvert.SerializeObject(latestrelease));
+            File.WriteAllText(path + "\\version.json", JsonConvert.SerializeObject(latestrelease,Formatting.Indented));
 
             DownloadStatus = true;
             System.Diagnostics.Process.Start(path);
